8ddb1ae6f044150205e9b3cc8f48a607b5631ef9
BalikaRahul <rr7511489@gmail.com>
2025-11-29 15:12:33 +0530

    updated files

---
+++ src/props/Fourth.jsx
@@ -0,0 +1,9 @@
+const Fourth = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  );
+};
+
+export default Fourth;
---
+++ src/props/Second.jsx
@@ -0,0 +1,9 @@
+const Second = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  );
+};
+
+export default Second;
